e2772840179fe294e836f9d491b952a92bec92ee
little kid <133933012+vixho69@users.noreply.github.com>
2023-05-18 03:04:41 +0000

    gen-apodo.py
    idea de ale :D

---
+++ gen-apodo.py
@@ -0,0 +1,17 @@
+import random
+import os
+while True:
+    dd = int(input("¿cuantas veces quieres mesclar el apodo?: "))
+    gg = int(input("¿cuantas veces quieres repetir?: "))
+    os.system("cls" if os.name == "nt" else "clear")
+    for gg in range(gg):
+        ss = ["pablito","tutoxica","uwu","123","owo","lul","ola","pato",":O","noc","yokse","XD","X","D","paz","eso","tilin","gato","ye","a","O","o"]
+        print("")
+        ks = "".join(random.sample(ss, dd))
+        print(f"{ks}\n")
+    print("¿quieres volver a generar?")
+    jh = input("ingresa (s/n): ").lower()
+    os.system("cls" if os.name == "nt" else "clear")
+    if jh != "s":
+        print("adios!")
+        break
